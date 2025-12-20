5749b48a2897b75cc9a550858a6a0e2c71d8cbad
Narathip1707 <narathip170747@gmail.com>
2025-12-20 20:04:15 +0700

    Add AbstractFactory Method pattern implementation with buildAquarium

---
+++ project/AbstractFactory/abstract_factory.cs
@@ -0,0 +1,172 @@
+﻿using System;
+namespace Task02.AbstractFactoryMethod.buildAquarium 
+{
+
+    // Product
+    public interface Fish
+    {
+        void Setup();
+    }
+    public interface Plant
+    {
+        void Setup();
+    }
+    public interface Substrate
+    {
+        void Setup();
+    }
+    // ConcreteProducts
+    //--------------
+    // Freshwater(น้ำจืด)
+    public class FreshwaterFish : Fish
+    {
+        public void Setup(){Console.WriteLine("ใส่ปลาน้ำจืดแล้ว");}
+    }
+    public class FreshwaterPlant : Plant
+    {
+        public void Setup(){Console.WriteLine("ใส่พืชน้ำจืดแล้ว");}
+    }
+    public class FreshwaterSubstrate : Substrate
+    {
+        public void Setup(){Console.WriteLine("ใส่รองพื้นตู้ปลาสำหรับปลาน้ำจืดแล้ว");}
+    }
+    // Saltwater(น้ำเค็ม)
+    public class SaltwaterFish : Fish
+    {
+        public void Setup(){Console.WriteLine("ใส่ปลาน้ำเค็มแล้ว");}
+    }
+    public class SaltwaterPlant : Plant
+    {
+        public void Setup(){Console.WriteLine("ใส่พืชน้ำเค็มแล้ว");}
+    }
+    public class SaltwaterSubstrate : Substrate
+    {
+        public void Setup(){Console.WriteLine("ใส่รองพื้นตู้ปลาสำหรับปลาน้ำเค็มแล้ว");}
+    }
+    // Brackish(น้ำกร่อย)
+    public class BrackishFish : Fish
+    {
+        public void Setup(){Console.WriteLine("ใส่ปลาน้ำกร่อยแล้ว");}
+    }
+    public class BrackishPlant : Plant
+    {
+        public void Setup(){Console.WriteLine("ใส่พืชน้ำกร่อยแล้ว");}
+    }
+    public class BrackishSubstrate : Substrate
+    {
+        public void Setup(){Console.WriteLine("ใส่รองพื้นตู้ปลาสำหรับปลาน้ำกร่อยแล้ว");}
+    }
+    // AbstactFactory
+    public interface AquariumFactory
+    {
+        Fish CreateFish();
+        Plant CreatePlant();
+        Substrate CreateSubstrate();
+    }
+    // ConcreteFactory
+    public class FreshwaterFactory : AquariumFactory
+    {
+        public Fish CreateFish()
+        {
+            return new FreshwaterFish();
+        }
+        public Plant CreatePlant()
+        {
+            return new FreshwaterPlant();
+        }
+        public Substrate CreateSubstrate()
+        {
+            return new FreshwaterSubstrate();
+        }
+    }
+    public class SaltwaterFactory : AquariumFactory
+    {
+        public Fish CreateFish()
+            {
+                return new SaltwaterFish();
+            }
+        public Plant CreatePlant()
+            {
+                return new SaltwaterPlant();
+            }
+        public Substrate CreateSubstrate()
+            {
+                return new SaltwaterSubstrate();
+            }
+    }
+    public class BrackishFactory : AquariumFactory
+    {
+        public Fish CreateFish()
+        {
+            return new BrackishFish();
+        }
+        public Plant CreatePlant()
+        {
+            return new BrackishPlant();
+        }
+        public Substrate CreateSubstrate()
+        {
+            return new BrackishSubstrate();
+        }
+    }
+    // Client Class
+    public class Client
+    {
+        private AquariumFactory factory;
+        private Fish fish;
+        private Plant plant;
+        private Substrate substrate;
+
+        public Client(AquariumFactory f)
+        {
+            factory = f;
+            CreateAquarium();
+        }
+        public void CreateAquarium()
+        {
+            fish = factory.CreateFish();
+            plant = factory.CreatePlant();
+            substrate = factory.CreateSubstrate();
+        }
+        public void Setup()
+        {
+            fish.Setup();
+            plant.Setup();
+            substrate.Setup();
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            AquariumFactory factory;
+            Client client;
+            //น้ำจืด
+            factory = new FreshwaterFactory();
+            client = new Client(factory);
+            client.Setup();
+            Console.WriteLine("======");
+            //น้ำเค็ม
+            factory = new SaltwaterFactory();
+            client = new Client(factory);
+            client.Setup();
+            Console.WriteLine("======");
+            //น้ำกร่อย
+            factory = new BrackishFactory();
+            client = new Client(factory);
+            client.Setup();
+            Console.WriteLine("======");
+
+            Console.WriteLine("Call in Main");
+            factory = new FreshwaterFactory();
+            Fish fish = factory.CreateFish();
+            Plant plant = factory.CreatePlant();
+            Substrate substrate = factory.CreateSubstrate();
+            fish.Setup();
+            plant.Setup();
+            substrate.Setup();
+
+        }
+    }
+}
